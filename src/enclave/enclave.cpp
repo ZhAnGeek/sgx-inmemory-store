ae0f21971e24790f79ba2e6f185af86408e0edca
zhangsk01 <zhangsk01@fenbi.com>
2021-11-07 21:56:16 +0800

    ADD: encryption

--- src/enclave/enclave.cpp
+++ src/enclave/enclave.cpp
@@ -92,15 +92,84 @@ int ecall_set_key(const char* pk, const char* nonce, uint8_t* val, uint32_t val_
 
     const std::string ptk = std::string(plain_token);
     user_key_map[ptk] = std::string(plain); // store
-    memcpy(token, ptk.c_str(), ptk.length()); // ret token
+
+    // create buffer
+    uint32_t ptk_cipher_len = ptk.length() + SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE;
+    uint8_t ptk_token[ptk_cipher_len];
+    // gen rnd iv
+    sgx_read_rand(ptk_token, SGX_AESGCM_IV_SIZE);
+
+    // encrypt
+    sgx_rijndael128GCM_encrypt(shared_dhkey, ptk.c_str(), ptk.length(),
+        ptk_token + SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE, ptk_token, SGX_AESGCM_IV_SIZE, NULL, 0,
+        (sgx_aes_gcm_128bit_tag_t *)(ptk_token + SGX_AESGCM_IV_SIZE));
+    memcpy(token, ptk_token, ptk_cipher_len); // ret token
     return SGX_SUCCESS;
 }
 
 int ecall_get_key(const char*pk, const char* token, uint8_t* val, uint32_t max_val_len, uint8_t* signature, uint32_t max_sig_len, uint32_t* val_len, uint32_t* sig_len) {
-    // const std::string keyVal = std::string(key);
-    // const std::string insideVal = user_key_map[keyVal];
-    // memcpy(val, insideVal.c_str(), insideVal.length());
-    return 0;
+    sgx_ec256_public_t client_pk = {0};
+
+    uint8_t *pk_bytes = (uint8_t *)pk;
+    bytes_swap(pk_bytes, 32);
+    bytes_swap(pk_bytes + 32, 32);
+    memcpy(&client_pk, pk_bytes, sizeof(sgx_ec256_public_t));
+
+    sgx_ec256_dh_shared_t shared_dhkey;
+
+    sgx_ecc_state_handle_t ecc_handle = NULL;
+    sgx_ecc256_open_context(&ecc_handle);
+    int sgx_ret = sgx_ecc256_compute_shared_dhkey(&enclave_sk, &client_pk, &shared_dhkey, ecc_handle);
+    if (sgx_ret != SGX_SUCCESS) {
+        return sgx_ret;
+    }
+    sgx_ecc256_close_context(ecc_handle);
+    bytes_swap(&shared_dhkey, 32);
+
+    sgx_sha256_hash_t h;
+    sgx_sha256_msg((const uint8_t *)&shared_dhkey, sizeof(sgx_ec256_dh_shared_t), (sgx_sha256_hash_t *)&h);
+
+    sgx_aes_gcm_128bit_key_t key;
+    memcpy(key, h, sizeof(sgx_aes_gcm_128bit_key_t));
+
+    char* insideVal = new char[val_len];
+    memcpy(insideVal, val, val_len);
+    
+    std::string _cipher = base64_decode(token);
+    uint8_t *cipher = (uint8_t *)_cipher.c_str();
+    int cipher_len = _cipher.size();
+
+    uint32_t needed_size = cipher_len - SGX_AESGCM_IV_SIZE - SGX_AESGCM_MAC_SIZE;
+    // need one byte more for string terminator
+    char plain[needed_size + 1];
+    plain[needed_size] = '\0';
+
+    // decrypt
+    sgx_ret = sgx_rijndael128GCM_decrypt(&key,
+        cipher + SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE,          /* cipher */
+        needed_size, (uint8_t *)plain,                              /* plain out */
+        cipher, SGX_AESGCM_IV_SIZE,                                 /* nonce */
+        NULL, 0,                                                    /* aad */
+        (sgx_aes_gcm_128bit_tag_t *)(cipher + SGX_AESGCM_IV_SIZE)); /* tag */
+    if (sgx_ret != SGX_SUCCESS) {
+        return sgx_ret;
+    }
+    
+    const std::string keyVal = std::string(plain);
+    const std::string insideVal = user_key_map[plain];
+
+    // create buffer
+    uint32_t ptv_cipher_len = insideVal.length() + SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE;
+    uint8_t ptv_token[ptv_cipher_len];
+    // gen rnd iv
+    sgx_read_rand(ptv_token, SGX_AESGCM_IV_SIZE);
+
+    // encrypt
+    sgx_rijndael128GCM_encrypt(shared_dhkey, insideVal.c_str(), insideVal.length(),
+        ptv_token + SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE, ptv_token, SGX_AESGCM_IV_SIZE, NULL, 0,
+        (sgx_aes_gcm_128bit_tag_t *)(ptv_token + SGX_AESGCM_IV_SIZE));
+    memcpy(val, ptv_token, ptv_cipher_len); // ret token
+    return SGX_SUCCESS;
 }
 
 // returns enclave pk in Big Endian format
