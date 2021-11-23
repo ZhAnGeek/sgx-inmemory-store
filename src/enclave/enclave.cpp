e94e4d85dfe95c636e008652e9114ad085e0f8af
zhangsk01 <zhangsk01@fenbi.com>
2021-11-23 18:09:29 +0800

    FIX: try

--- src/enclave/enclave.cpp
+++ src/enclave/enclave.cpp
@@ -25,11 +25,11 @@ void bytes_swap(void *bytes, size_t len)
 int ecall_set_key(const char* pk, const char* nonce, uint8_t* val, uint32_t val_len, uint8_t* signature, uint8_t* token, uint32_t* sig_len, uint32_t* tok_len) {
     sgx_ec256_public_t client_pk = {0};
 
-    uint8_t *pk_bytes = (uint8_t *)pk;
-    bytes_swap(pk_bytes, 32);
-    bytes_swap(pk_bytes + 32, 32);
-    memcpy(&client_pk, pk_bytes, sizeof(sgx_ec256_public_t));
-    memcpy(&token, &client_pk, sizeof(sgx_ec256_public_t));
+    // uint8_t *pk_bytes = (uint8_t *)pk;
+    // bytes_swap(pk_bytes, 32);
+    // bytes_swap(pk_bytes + 32, 32);
+    // memcpy(&client_pk, pk_bytes, sizeof(sgx_ec256_public_t));
+    memcpy(&token, (uint8_t *)pk, sizeof(sgx_ec256_public_t));
 
     // sgx_ec256_dh_shared_t shared_dhkey;
 
