21cda25476e7e6bd41e067eb3e60a4d65cd9087e
zhangsk01 <zhangsk01@fenbi.com>
2021-11-25 01:07:13 +0800

    FIX: try

--- src/enclave/enclave.cpp
+++ src/enclave/enclave.cpp
@@ -55,16 +55,15 @@ int ecall_set_key(const char* pk, const char* nonce, uint8_t* val, uint32_t val_
     char plain[needed_size + 1];
     plain[needed_size] = '\0';
 
-    // decrypt
-    sgx_ret = sgx_rijndael128GCM_decrypt(&key,
-        cipher + SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE,          /* cipher */
-        needed_size, (uint8_t *)plain,                              /* plain out */
-        cipher, SGX_AESGCM_IV_SIZE,                                 /* nonce */
-        NULL, 0,                                                    /* aad */
-        (sgx_aes_gcm_128bit_tag_t *)(cipher + SGX_AESGCM_IV_SIZE)); /* tag */
-    if (sgx_ret != SGX_SUCCESS) {
-        return sgx_ret;
-    }
+    // sgx_ret = sgx_rijndael128GCM_decrypt(&key,
+    //     cipher + SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE,          /* cipher */
+    //     needed_size, (uint8_t *)plain,                              /* plain out */
+    //     cipher, SGX_AESGCM_IV_SIZE,                                 /* nonce */
+    //     NULL, 0,                                                    /* aad */
+    //     (sgx_aes_gcm_128bit_tag_t *)(cipher + SGX_AESGCM_IV_SIZE)); /* tag */
+    // if (sgx_ret != SGX_SUCCESS) {
+    //     return sgx_ret;
+    // }
 
     // needed_size = cipher_len - SGX_AESGCM_IV_SIZE - SGX_AESGCM_MAC_SIZE;
     // // need one byte more for string terminator
